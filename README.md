76c1f7c7da772f85e148e735684db80b2b792938
Laurent Demailly <ldemailly@gmail.com>
2025-04-14 11:54:29 -0700

    It works, nice

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # webscreenshot
 Take a screenshot of a Web page after giving it a delay for rendering
+
+## Install
+```bash
+go install github.com/fortio/webscreenshot@latest
+```
+
+## Usage
+```bash
+webscreenshot [-delay duration] URL > screenshot.png
+```
+
+e.g
+```bash
+webscreenshot -delay 5s https://demo.fortio.org/browse?url=qps_max-s1_to_s2-0.7.1-2018-04-05-22-04.json > screenshot.png
+open screenshot.png
+```
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"os"
+	"time"
+
+	"fortio.org/cli"
+	"fortio.org/log"
+	"github.com/chromedp/chromedp"
+)
+
+func main() {
+	os.Exit(Main())
+}
+
+func Main() int {
+	delayFlag := flag.Duration("delay", 1*time.Second, "Delay before the screen shot")
+	cli.MinArgs = 1
+	cli.ArgsHelp = "URL > screenshot.png\n" +
+		"to take a screenshot of a web page using Chrome after a given delay"
+	cli.Main()
+	url := flag.Arg(0)
+	delay := *delayFlag
+	var res []byte
+	log.Printf("Taking screenshot of %s after %s", url, delay)
+	ctx, cancel := chromedp.NewContext(
+		context.Background(),
+		// chromedp.WithDebugf(log.Printf),
+	)
+	defer cancel()
+
+	err := chromedp.Run(ctx, chromedp.Tasks{
+		chromedp.Navigate(url),
+		chromedp.ActionFunc(func(ctx context.Context) error {
+			time.Sleep(delay)
+			return nil
+		}),
+		chromedp.FullScreenshot(&res, 100),
+	})
+	if err != nil {
+		return log.FErrf("Failed to take screenshot: %v", err)
+	}
+	os.Stdout.Write(res)
+	return 0
+}
